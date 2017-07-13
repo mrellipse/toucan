4ad2d67c64b8c913e30d420475f9ee38bcd67fcf
mrellipse <mrellipse@gmail.com>
2017-07-13 18:44:05 +1000

    minor css facelift

--- src/ui/app/vuelidate.d.ts
+++ src/ui/app/vuelidate.d.ts
@@ -3,7 +3,7 @@ declare module "vuelidate" {
     import _Vue = require("vue");
 
     /**
-     * @module augmentation to ComponentOptions defined by Vuejs
+     * @module augmentation to ComponentOptions defined by VueJs
     */
     module "vue/types/options" {
 
